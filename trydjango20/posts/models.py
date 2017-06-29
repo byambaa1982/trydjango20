24feaceebbf157dc5c6089dd87ac6be0bcdc9b07
Byamba Enkhbat <byambaa1982@gmail.com>
2017-06-29 13:11:13 -0700

    make migration

---
+++ trydjango20/posts/models.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals
+from django.db import models
+from django.core.urlresolvers import reverse
+
+# Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=120)
+    content=models.TextField()
+    updated=models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp=models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def _inicode_(self): 
+        return self.title
+
+    def _str_(self): 
+        return self.title
