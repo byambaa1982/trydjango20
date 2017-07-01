825a71b59b320be53ddc5c24f4ba4b0cb9878b8a
Byamba Enkhbat <byambaa1982@gmail.com>
2017-07-01 09:44:04 -0700

    customized admin

--- trydjango20/posts/admin.py
+++ trydjango20/posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+class PostModelAdmin(admin.ModelAdmin):  
+	list_display = ["title", "updated", "timestamp"]  
+ 	list_display_links = ["updated"]  
+	list_editable = ["title"]  	list_filter = ["updated", "timestamp"]  
+
+admin.site.register(Post)  No newline at end of file  
+	search_fields = ["title", "content"]  
+	class Meta:  
+ 			model = Post  
