1d43075323f4edbbca951be595f7bc1a595d6a03
Byamba Enkhbat <byambaa1982@gmail.com>
2017-07-07 18:41:52 -0700

    class 17 query set and 404

--- trydjango20/posts/views.py
+++ trydjango20/posts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post 
 # Create your views here.
+def post_list(request):
+	queryset = Post.objects.all()
+	context = {
+		"object_list": queryset,
+		"title": "List"
+	}
+	
+	return render(request, "index.html", context)
+def post_create(request):
+	return HttpResponse("<h1>create byamba</h1>")
+def post_detail(request):
+	#instance = Post.objects.get(id=1)
+	instance = get_object_or_404(Post, id=3)
+	context = {
+		"title": instance.title,
+		"instance": instance,
+	}
+	return render(request, "index.html", context)
+def post_update(request):
+ 	return HttpResponse("<h1>Hi Byambaa</h1>")
+def post_delete(request):
+ 	return HttpResponse("<h1>Delete</h1>")
