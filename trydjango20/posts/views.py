1ffdd3890885fab8c645cbe2399bbed317495091
Byamba Enkhbat <byambaa1982@gmail.com>
2017-07-09 17:48:46 -0700

    Create view

--- trydjango20/posts/views.py
+++ trydjango20/posts/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from .models import Post 
+from .models import Post
+from .forms import PostForm 
 # Create your views here.
 def post_list(request):
 	queryset = Post.objects.all()
@@ -11,7 +12,15 @@ def post_list(request):
 	
 	return render(request, "index.html", context)
 def post_create(request):
-	return HttpResponse("<h1>create byamba</h1>")
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		print (form.cleaned_data.get("title"))
+		instance.save()
+	context = {
+		"form": form,
+	}
+	return render(request, "post_form.html", context)
 def post_detail(request, id=None):
 	#instance = Post.objects.get(id=1)
 	instance = get_object_or_404(Post, id=id)
