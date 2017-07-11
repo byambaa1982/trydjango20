b74f6178b3fd8cbc5da312ad3d4a999d0f48393a
Byamba Enkhbat <byambaa1982@gmail.com>
2017-07-11 12:50:34 -0700

    setup static files

--- trydjango20/trydjango20/urls.py
+++ trydjango20/trydjango20/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.conf import settings
+from django.conf.urls.static import static 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^posts/', include("posts.urls", namespace='posts')),
     #url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
