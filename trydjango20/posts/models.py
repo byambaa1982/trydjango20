53e92cfb85c3fcda1efa585ccf939347637c2a2b
Byamba Enkhbat <byambaa1982@gmail.com>
2017-07-17 10:57:13 -0700

    create slug

--- trydjango20/posts/models.py
+++ trydjango20/posts/models.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.core.urlresolvers import reverse
-
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
 
 # Create your models here.
 def upload_location(instance, filename):
@@ -9,6 +10,7 @@ def upload_location(instance, filename):
     return "%s/%s" %(instance.id, filename)
 class Post(models.Model):
     title=models.CharField(max_length=120)
+    slug = models.SlugField(unique=True)
     image = models.ImageField(
         upload_to=upload_location, 
         null=True, 
@@ -30,3 +32,19 @@ class Post(models.Model):
 	    return reverse("posts:detail", kwargs={"id": self.id})
     class Meta:
         ordering = ["-timestamp", "-updated"]
+
+def create_slug(instance, new_slug=None):
+    slug = slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    qs = Post.objects.filter(slug=slug).order_by("-id")
+    exists = qs.exists()
+    if exists:
+        new_slug = "%s-%s" %(slug, qs.first().id)
+        return create_slug(instance, new_slug=new_slug)
+    return slug
+def  pre_save_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
