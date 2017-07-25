57b54617d97c6503810483508559daf647b044ca
Byamba Enkhbat <byambaa1982@gmail.com>
2017-07-25 13:51:00 -0700

    shareing social network

--- trydjango20/posts/views.py
+++ trydjango20/posts/views.py
@@ -1,3 +1,4 @@
+from urllib.parse import quote_plus
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
@@ -37,9 +38,11 @@ def post_create(request):
 def post_detail(request, id=None):
 	#instance = Post.objects.get(id=1)
 	instance = get_object_or_404(Post, id=id)
+	share_string = quote_plus(instance.content)
 	context = {
 		"title": instance.title,
 		"instance": instance,
+		"share_string": share_string,
 	}
 	return render(request, "post_detail.html", context)
 def post_update(request, id=None):
