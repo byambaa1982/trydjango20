3b42d6561dc88c8d41fe1da8c3e283504e9d0a80
Byamba Enkhbat <byambaa1982@gmail.com>
2017-07-29 12:50:11 -0700

    basic user permitions

--- trydjango20/posts/views.py
+++ trydjango20/posts/views.py
@@ -25,6 +25,8 @@ def post_list(request):
 	
 	return render(request, "post_list.html", context)
 def post_create(request):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
@@ -46,6 +48,8 @@ def post_detail(request, id=None):
 	}
 	return render(request, "post_detail.html", context)
 def post_update(request, id=None):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	instance = get_object_or_404(Post, id=id)
 	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
 	if form.is_valid():
@@ -60,6 +64,8 @@ def post_update(request, id=None):
 	}
 	return render(request, "post_form.html", context)
 def post_delete(request, id=None):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	instance = get_object_or_404(Post, id=id)
 	instance.delete()
 	messages.success(request, "Successfully deleted")
